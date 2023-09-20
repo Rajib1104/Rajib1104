580f9764e18f4fb3fff05919aac588ef07354a68
Rajib samanat <145602292+Rajib1104@users.noreply.github.com>
2023-09-20 23:41:54 +0530

    Create Income-tax-cc-code

---
+++ Income-tax.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main()
+{
+    float income ,tax=0.0;
+    printf("Enter your income \n");
+    scanf("%f",&income);
+
+    if (income>=250000 && income<=500000)
+    {
+        tax = tax + 0.05 *(income-250000);
+    }
+    else if (income >=500000 && income<=1000000)
+    {
+        tax = tax + 0.20 *(income-500000);
+    }
+   else if(income>=1000000)
+    {
+        tax = tax + 0.30 * (income-1000000);
+    }
+    printf("Your income tax is %f", tax);
+    return 0;
+}
